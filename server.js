108246d457fc639d1a7ac69e1511e47b7b59cf93
Tosin Quadri <tquadri@softalliance.com>
2022-06-02 09:12:04 +0100

    add vue

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 require("dotenv").config(); // ALLOWS ENVIRONMENT VARIABLES TO BE SET ON PROCESS.ENV SHOULD BE AT TOP
 const cors = require('cors');
 const express = require("express");
+const serveStatic = require("serve-static")
+const path = require('path');
+
+
 const app = express();
 const corsOptions ={
    origin:'*', 
@@ -14,6 +18,7 @@ app.use(express.json({limit: '50mb'})); // parse json bodies in the request obje
 // Redirect requests to endpoint starting with /posts to postRoutes.js
 app.use("/movies", require("./routes/moviesRoutes"));
 app.use("/user", require("./routes/userRoutes"));
+app.use(serveStatic(path.join(__dirname, 'dist')));
 
 app.use(express.static('public'))
 
